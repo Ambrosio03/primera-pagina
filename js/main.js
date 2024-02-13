170ac25c03a386ea1058c80ee9e85ba36e5fe8cb
Ambrosio03 <ambrosio0304@gmail.com>
2024-02-13 11:09:35 +0100

    primer commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtener todos los enlaces de servicios
+    
     var servicioLinks = document.querySelectorAll('.servicio-link');
-  
-    // Iterar sobre cada enlace de servicio
     servicioLinks.forEach(function(link) {
-      // Agregar evento clic a cada enlace
       link.addEventListener('click', function(event) {
-        // Prevenir el comportamiento predeterminado del enlace
         event.preventDefault();
-        
-        // Obtener la URL de destino del enlace
         var destino = this.getAttribute('href');
-        
-        // Redireccionar a la página de detalles del servicio
         window.location.href = destino;
       });
     });
